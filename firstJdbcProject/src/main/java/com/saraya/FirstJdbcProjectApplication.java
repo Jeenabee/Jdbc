2a3aed1c60df8a2930fa61a2459fdb0234afbcce
Jeenabee <59122399+Jeenabee@users.noreply.github.com>
2021-09-20 16:02:58 +0000

    Add files via upload

---
+++ firstJdbcProject/src/main/java/com/saraya/FirstJdbcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.saraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJdbcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJdbcProjectApplication.class, args);
+	}
+
+}
